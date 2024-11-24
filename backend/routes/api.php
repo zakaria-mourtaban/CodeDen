5a9cc6cda9e7fa05a752bc0ce6d9a7e60788f667
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-11-24 17:05:01 +0200

    Add: File Controller, Migration And File Model

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,13 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\FileController;
+
+
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
 
-Route::get('/user', function () {
-    echo "Hello World";
-});
+// Route::get("/hello",[UserController::class, "hello_world"]);
 
 
