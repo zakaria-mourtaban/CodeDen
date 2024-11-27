1983695be8e71d900ca6805846e782c4fb49feb2
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-11-27 15:37:56 +0200

    React <> Add: Sidebar Component

---
+++ frontend/src/components/base/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Sidebar.css";
+
+const Sidebar = () => (
+    <div className="sidebar">
+      <h3>Workspaces</h3>
+      <ul>
+        <li>Workspace 1</li>
+        <li>Workspace 2</li>
+        <li>Workspace 3</li>
+      </ul>
+    </div>
+  );
+  
+  export default Sidebar;
+  
