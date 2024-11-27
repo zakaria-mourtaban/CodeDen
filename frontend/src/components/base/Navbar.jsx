5e5dbafab9c4afce88c4c345f4f3dda28ec16cc0
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-11-27 15:36:06 +0200

    React <> Add: Navbar Component

---
+++ frontend/src/components/base/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Navbar.css";
+
+
+const Navbar = () => (
+    <nav className="navbar">
+      <h1>Code Workspace</h1>
+    </nav>
+  );
+  
+export default Navbar;
