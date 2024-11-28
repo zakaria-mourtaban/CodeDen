dde99a68838f8246eceaf5fcc395a345225bbfb2
fh95-tech <fakih.hussein995@gmail.com>
2024-11-28 04:12:10 +0200

    added store function

--- backend/app/Http/Controllers/CollaborationController.php
+++ backend/app/Http/Controllers/CollaborationController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Collaboration;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
-class CollaborationController extends Controller
-{
+class CollaborationController extends Controller{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'permission' => 'required|in:view,edit',
+        ]);
+
+        $receiver = User::where('email', $request->email)->first();
+
+        $collaboration = Collaboration::create([
+            'sender_id' => Auth::id(),
+            'receiver_id' => $receiver ? $receiver->id : null,
+            'email' => $request->email,
+            'permission' => $request->permission,
+        ]);
+
+       
+    }
+
     
 }
